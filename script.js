ed265455030f5a004722eb5a8f69ae2f7f10cb27
Kuba-David <jakub.david@hotmail.com>
2025-04-03 20:50:59 +0200

    carousel and form addition

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ const nav = document.querySelector("nav");
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("show");
     hamburger.classList.toggle("active");
-    })
+})
+
+
+const modal = document.getElementById("modal")
+const openModalBtn = document.getElementById("openModal")
+const closeModalBtn = document.querySelector(".close")
+
+openModalBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+    document.body.classList.add("modal-opened");
+})
+
+closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.body.classList.remove("modal-opened")
+})
